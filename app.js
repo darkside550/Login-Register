f60d02f2ba8189a5f86b12883a59da70413195f8
darkside550 <44047478+darkside550@users.noreply.github.com>
2019-12-13 02:05:18 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose =require('mongoose');
+const flash =require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+
+//passport config
+require('./config/passport')(passport);
 
 //DB config
 const db=require('./config/keys').MongoURI
@@ -18,6 +24,30 @@ app.set('view engine','ejs');
 
 //BodyParser
 app.use(express.urlencoded({extended: false}));
+
+//express session
+app.use(session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    }));
+
+//passport middleware    
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Coonect flash
+app.use(flash());
+
+//Global vars
+app.use((req,res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+
+    next();
+});
+
 
 //Routes
 app.use('/', require('./routes/index'));
